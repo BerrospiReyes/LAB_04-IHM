91aa9680959ef0375f5ab1ab9e77fee8f4bf0d5f
hernan10m <167214755+hernan10m@users.noreply.github.com>
2024-04-16 23:35:34 +0000

    final

--- src/aside.js
+++ src/aside.js
@@ -1,15 +1,43 @@
 const Aside = () => {
-    return <aside>
-<div>
-  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-</div>
-<div class="mb-3">
-  <label for="exampleInputPassword1" class="form-label">Password</label>
-  <input type="password" class="form-control" id="exampleInputPassword1" />
-</div>
-
-    </aside>;
+  return (
+      <aside>
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-12 bg-info d-flex flex-column align-items-center justify-content-center p-3">
+                      <h1>📒Registro📒</h1>
+                      <form>
+                          <div className="form-group">
+                              <label htmlFor="usuario">Nombre de usuario:</label>
+                              <input type="text" id="usuario" />
+                          </div>
+                          <div>
+                              <br />
+                              <label htmlFor="direccion">Dirección:</label>
+                              <input type="text" id="direccion" />
+                          </div>
+                          <div>
+                              <br />
+                              <label htmlFor="email">E-mail</label>
+                              <input type="text" id="email" />
+                          </div>
+                          <div>
+                              <br />
+                              <label htmlFor="numero">Ingrese edad</label>
+                              <input type="number" id="numero" />
+                          </div>
+                          <div>
+                              <br />
+                              <label htmlFor="contraseña">Contraseña</label>
+                              <input type="password" id="contraseña" />
+                          </div>
+                          <br />
+                          <button type="submit" className="btn btn-dark" tabIndex="5">Enviar</button>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </aside>
+  );
 }
 
 export default Aside;
--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,17 @@
 const Footer = () => {
     return <footer>
-        <p>footer</p>
+  <div class="container p-3">
+      <div class="row">
+        <div class="col-md-3 text-center text-bg-dark m-2 p-3">
+          <p>Hernán Mauriccio Gastón Berrospi Reyes</p></div>
+        <div class="col-md-3 text-center text-bg-dark m-2 p-3">
+          <p>Correo electrónico: mauriccioreyes@outlook.com</p></div>
+      <div class="col-md-2 text-center text-bg-dark m-2 p-3">
+          <p>Teléfono: 941498415</p></div>
+      <div class="col-md-3 text-center text-bg-dark m-2 p-3">
+          <p>Dirección: Calle José Santos Chocano, Trujillo, Perú</p></div>
+      </div>
+  </div>
     </footer>;
 }
 
